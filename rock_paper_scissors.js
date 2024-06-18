2542ce39fd8eb8c64a55369f410ba0f18adc03ca
cyril-hhf <cyrilhhf@gmail.com>
2024-06-18 20:23:53 +0800

    Single Round function playRound is written and tested

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -2,17 +2,48 @@ outcome = ["rock", "paper", "scissors"]
 
 function getComputerChoice() {
     computerChoice  = outcome[Math.floor(Math.random() * outcome.length)]
-    console.log(computerChoice)
+    return computerChoice
 }
 
-getComputerChoice()
-
-function getHumanChoice(input) {
+function getHumanChoice() {
     humanChoice = prompt("Enter rock or paper or scissors.")
-    console.log(humanChoice)
+    return humanChoice.toLowerCase()
 }
 
-getHumanChoice()
+const computerSelection = getComputerChoice()
+const humanSelection = getHumanChoice()
 
 humanScore = 0;
 computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "rock" && computerChoice === "rock") {
+            alert("It's a draw!")
+        } else if (humanChoice === "paper" && computerChoice === "paper") {
+            alert("It's a draw!")
+        } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+            alert("It's a draw!")
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            alert("You lose this round!")
+            computerScore = computerScore + 1
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            alert("You lose this round!")
+            computerScore = computerScore + 1
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            alert("You lose this round!")
+            computerScore = computerScore + 1
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            alert("You win!")
+            humanScore = humanScore + 1
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            alert("You win!")
+            humanScore = humanScore + 1
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            alert("You win!")
+            humanScore = humanScore + 1
+        }
+    }
+
+playRound(humanSelection, computerSelection)
+console.log(humanScore)
+console.log(computerScore)
