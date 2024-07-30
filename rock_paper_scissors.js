c4edf4a9440bdd64a173261ab6ce0a63077c4eb4
cyril-hhf <cyrilhhf@gmail.com>
2024-07-30 17:07:48 +0800

    Created UI

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,17 +1,15 @@
-outcome = ["rock", "paper", "scissors"]
+const outcome = ["rock", "paper", "scissors"]
+
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
+const maxScore = 3;
 
 // Get computer's choice
 function getComputerChoice() {
     computerChoice  = outcome[Math.floor(Math.random() * outcome.length)]
     return computerChoice
 }
-
-// Get player's choice
-function getHumanChoice() {
-    humanChoice = prompt("Enter rock or paper or scissors.")
-    return humanChoice.toLowerCase()
-}
-
 
 
 // Play single round
@@ -24,12 +22,62 @@ function playRound(humanChoice, computerChoice) {
       (humanChoice === "paper" && computerChoice === "rock") ||
       (humanChoice === "scissors" && computerChoice === "paper")
     ) {
+      playerScore++;
       return "You win!";
     } else {
+      computerScore++;
       return "You lose!";
     }
   }
 
+  function updateScoreboard() {
+    document.getElementById("player-score").textContent = playerScore;
+    document.getElementById("computer-score").textContent = computerScore;
+}
+
+function handleClick(event) {
+  round ++;
+
+  const humanChoice = event.target.id.replace("-button", ""); // Get the player's choice based on the button's ID
+  const computerChoice = getComputerChoice();
+  const result = playRound(humanChoice, computerChoice);
+  document.getElementById("result-message").textContent = `You chose ${humanChoice}, computer chose ${computerChoice}. ${result}`;
+  updateScoreboard();
+
+  if (playerScore === maxScore | computerScore === maxScore) {
+    endGame()
+  }
+}
+
+function endGame() {
+  rock.removeEventListener("click", handleClick);
+  paper.removeEventListener("click", handleClick);
+  scissors.removeEventListener("click", handleClick);
+
+  let finalMessage = "Game over! ";
+  if (playerScore > computerScore) {
+    finalMessage += "You won the game!";
+  } else if (playerScore < computerScore) {
+    finalMessage += "You lost the game!"
+  }
+
+  const finalResultElement = document.createElement("p");
+  finalResultElement.setAttribute("id", "final-result-message");
+  finalResultElement.textContent = finalMessage;
+  document.body.appendChild(finalResultElement);
+}
+
+
+const rock = document.getElementById("rock-button")
+const paper = document.getElementById("paper-button")
+const scissors = document.getElementById("scissors-button")
+
+rock.addEventListener("click", handleClick);
+paper.addEventListener("click", handleClick);
+scissors.addEventListener("click", handleClick);
+
+
+/*
 // Play 5 rounds
 function playGame() {
     let humanScore = 0;
@@ -56,6 +104,12 @@ function playGame() {
     } 
 }
 
+// Get player's choice
+function getHumanChoice() {
+    humanChoice = prompt("Enter rock or paper or scissors.")
+    return humanChoice.toLowerCase()
+}
+*/
 
 
 // Run game
